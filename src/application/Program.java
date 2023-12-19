293b1722a095323230f8678aa9b7c59a80b4da40
Raphael Sena <rsenares1@gmail.com>
2023-12-19 11:31:38 -0300

    Implementing the enroll() method; Implementing the payTuition() method and cleaning up and finishing touches and creating an array of objects and showing them

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,33 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Student s1 = new Student();
-		// Ask how many users we want to add
+		Scanner sc = new Scanner(System.in);
+		
+	
+		
+		// Ask how many students we want to add
+		System.out.println("Enter number of new students to enroll: ");
+		int numberOfStudents = sc.nextInt();
+		Student[] student = new Student[numberOfStudents];
 		
 		// Create a number of new students
+		for (int i = 0; i < numberOfStudents; i++) {
+			student[i] = new Student();
+			student[i].enroll();
+			student[i].payTuition();
+		}
+		
+		for (int i = 0; i < numberOfStudents; i++) {
+			System.out.println(student[i].showInfo());
+		}
 		
 		
+		sc.close();
 	}
 
 }
--- src/application/Student.java
+++ src/application/Student.java
@@ -3,54 +3,84 @@ package application;
 import java.util.Scanner;
 
 public class Student {
-	
+
 	private String firstName;
 	private String lastName;
 	private Integer gradeYear;
 	private String studentId;
-	private String courses;
-	private Integer tuitionBalance;
+	private String courses = null;
+	private Integer tuitionBalance = 0;
 	private static Integer costOfCourse = 600;
 	private static int id = 1000;
-	
+
 	// Constructor: prompt user to enter student's name and year
 	public Student() {
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter student first name: ");
 		this.firstName = sc.nextLine();
-		
+
 		System.out.println("Enter student last name: ");
 		this.lastName = sc.nextLine();
-		
-		System.out.println("1 - Freshman"
-				+ "\n2 - Sophmore"
-				+ "\n3 - Junior"
-				+ "\n4 - Senior"
-				+ "\nEnter student class level: ");
+
+		System.out.println(
+				"1 - Freshman" + "\n2 - Sophmore" + "\n3 - Junior" + "\n4 - Senior" + "\nEnter student class level: ");
 		this.gradeYear = sc.nextInt();
-		
+
 		setStudentId();
-		
-		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentId);
-		
-	
 	}
-	
-	
+
 	// Generate an ID
 	private void setStudentId() {
 		// Grade level + ID
 		id++;
 		this.studentId = gradeYear + " " + id;
 	}
-	
+
 	// Enroll in courses
-	
-	// View balance 
-	
+	public void enroll() {
+		// Get inside a loop, user hits 0
+		int pointer = 0;
+		while (pointer == 0) {
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("Do you want to leave? (Q to quit)");
+			String leave = sc.nextLine();
+			if (leave.equals("Q")) {
+				System.out.println("BREAK!");
+				pointer++;
+			} else {
+				System.out.println("Enter course to enroll: ");
+				String course = sc.nextLine();
+
+				courses = courses + "\n  " + course;
+				tuitionBalance = tuitionBalance + costOfCourse;
+			}
+		}
+	}
+
+	// View balance
+	public void viewBalance() {
+		System.out.println("Your balance is: $" + tuitionBalance);
+	}
+
 	// Pay tuition
-	
+	public void payTuition () {
+		Scanner sc = new Scanner(System.in);
+		viewBalance();
+		System.out.println("Enter your payment: ");
+		int payment = sc.nextInt();
+		tuitionBalance -= payment;
+		System.out.println("Thank you for your payment of $" + payment);
+	}
+
 	// Show status
-	
-	
+	public String showInfo () {
+		return "Student Name: " + firstName + lastName
+				+ "\nGrade level: " + gradeYear
+				+ "\nStudent ID: " + studentId
+				+ "\nCourses: " + courses
+				+ "\nBalance: " + tuitionBalance;
+	}
+
 }
